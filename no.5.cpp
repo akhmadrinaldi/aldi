cf789dd4d3efbb3bdaa755b32d7ca6047e793546
akhmad rinaldi noor <akhmadrinaldinoor1998@gmail.com>
2017-11-08 18:13:24 +0700

    percobaan

--- no.5.cpp
+++ no.5.cpp
@@ -8,4 +8,9 @@ int main (){
 	
 	cout << "Masukkan tinggi badan dalam cm : ";
 	cin >> tinggi;
+	cout << "Masukkan berat badan dalam kg : ";
+	cin >> berat;
+	
+	if (berat<tinggi/2.5) {
+		cout << "Hasil : Kurus " <<endl;
 }
